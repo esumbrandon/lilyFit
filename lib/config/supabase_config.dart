ac1f207e3898706ed789f7c7f08743390375c98a
esum brandon <esumbrandon074@gmail.com>
2026-05-23 19:49:36 +0100

    Added supabase config file to the repo

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,5 @@
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://hfoarnikidnapqasppux.supabase.co';
+  static const String supabaseAnonKey =
+      'sb_publishable_px6tsQH9Mto3oF5aO2DJsw_-QgOERoB';
+}
