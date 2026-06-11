1ff21fbe7c0ec5cd45aadde4975c6e98bc9dec9e
esum brandon <esumbrandon074@gmail.com>
2026-06-11 20:16:11 +0100

    Fixed google sign-in.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "app.cypherwave.lilyfit"
+    namespace = "app.cipherwave.lilyfit"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.cypherwave.lilyfit"
+        applicationId = "app.cipherwave.lilyfit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
