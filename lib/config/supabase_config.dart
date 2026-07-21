b523e4e07efcad69a20b5da7c1d00dface19d449
esum brandon <esumbrandon074@gmail.com>
2026-07-21 17:43:16 +0100

    Minor updates.

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,5 +1,4 @@
 class SupabaseConfig {
   static const String supabaseUrl = 'https://hfoarnikidnapqasppux.supabase.co';
-  static const String supabaseAnonKey =
-      'sb_publishable_px6tsQH9Mto3oF5aO2DJsw_-QgOERoB';
+  static const String supabaseAnonKey = 'sb_publishable_px6tsQH9Mto3oF5aO2DJsw_-QgOERoB';
 }
